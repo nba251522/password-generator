6956a8c6380822e8b5d0b6a876123a044be036cc
nba251522 <inquister112345678@gmail.com>
2023-09-28 15:56:35 -0700

    refactored my code as it was getting hard to read. I also fixed my code so that the webpage was displaying a generated password once i entered all of my selections.

--- Develop/script.js
+++ Develop/script.js
@@ -3,64 +3,62 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?";
-var generatedPassword = "";
 
 function getRandomCharacter(string) {
-  return string.charAt(Math.floor(Math.random() * string.length));
+    return string.charAt(Math.floor(Math.random() * string.length));
 }
 
 function shuffleString(string) {
-  return string.split('').sort(function() { return 0.5 - Math.random() }).join('');
+    return string.split('').sort(function() { return 0.5 - Math.random() }).join('');
 }
 
 function generatePassword() {
-var length;
-var includeLowercase;                                                                             
-var includeUppercase;
-var includeNumber;
-var includeSpecialCharacter;
+    var generatedPassword = "";
+    var passwordLength, includeLowercase, includeUppercase, includeNumber, includeSpecialCharacter;
+    var numLowercase = 0, numUppercase = 0, numNumbers = 0, numSpecialCharacters = 0;
 
-while (true) {
-  var passwordLength = prompt("Enter password length between 8 and 128:");
-  if (passwordLength >= 8 && passwordLength <= 128) {
-      break;
-  }
-  alert("Password length must be between 8 and 128. Please enter a valid length.");
-}
+    while (true) {
+        passwordLength = prompt("Enter password length between 8 and 128:");
+        if (passwordLength >= 8 && passwordLength <= 128) {
+            break;
+        }
+        alert("Password length must be between 8 and 128. Please enter a valid length.");
+    }
 
-while (true) {
-includeLowercase = confirm("Include lowercase characters?");
-var numLowercase = 0; 
-if (includeLowercase) {
-    numLowercase = parseInt(prompt("Please enter the desired amount of lower case letters"));
-  }
+    while (true) {
+        includeLowercase = confirm("Include lowercase characters?");
+        if (includeLowercase) {
+            numLowercase = parseInt(prompt("Please enter the desired amount of lower case letters"));
+        }
 
-includeUppercase = confirm("Include uppercase characters?");
-  var numUppercase = 0;
-  if (includeUppercase) {
-      numUppercase = parseInt(prompt("Please enter the desired amount of upper case letters"));
-  }
-  
-includeNumber = confirm("Include numbers?");
-  var numNumbers = 0;
-  if (includeNumber) {
-      numNumbers = parseInt(prompt("Please enter the desired amount of numbers"));
-  }
- 
-includeSpecialCharacter = confirm("Include special characters?");
-  var numSpecialCharacters = 0;
-  if (includeSpecialCharacter) {
-      numSpecialCharacters = parseInt(prompt("Please enter the desired amount of special characters"));
-  }
+        includeUppercase = confirm("Include uppercase characters?");
+        if (includeUppercase) {
+            numUppercase = parseInt(prompt("Please enter the desired amount of upper case letters"));
+        }
 
-  var totalCharacters = numLowercase + numUppercase + numNumbers + numSpecialCharacters;
+        includeNumber = confirm("Include numbers?");
+        if (includeNumber) {
+            numNumbers = parseInt(prompt("Please enter the desired amount of numbers"));
+        }
 
-  if (totalCharacters === passwordLength) {
-      break; 
-  } else {
-      alert("The total number of characters (" + totalCharacters + ") does not match the password length (" + passwordLength + "). Please try again.");
-  }
-}
+        includeSpecialCharacter = confirm("Include special characters?");
+        if (includeSpecialCharacter) {
+            numSpecialCharacters = parseInt(prompt("Please enter the desired amount of special characters"));
+        }
+
+        if (!includeLowercase && !includeUppercase && !includeNumber && !includeSpecialCharacter) {
+            alert("You must select at least one character type.");
+            continue;
+        }
+
+        var totalCharacters = numLowercase + numUppercase + numNumbers + numSpecialCharacters;
+
+        if (totalCharacters === parseInt(passwordLength)) {
+            break;
+        } else {
+            alert("The total number of characters (" + totalCharacters + ") does not match the password length (" + passwordLength + "). Please try again.");
+        }
+    }
 
 for (var i = 0; i < numLowercase; i++) {
   generatedPassword += getRandomCharacter(lowercase);
